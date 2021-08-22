0ef3a86cbab42c3e7469e42c7d8c6d2029b11d30
Jova <jovaandrea8721@gmail.com>
2021-08-22 23:07:12 +0700

    adding weird animation

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,18 +10,18 @@ export const Footer = () => {
                 follow me
             </div>
             <div>
-                <IconButton href="https://www.instagram.com/jov.andrs/" target="_blank" rel="noreferer">
+                <IconButton className="social" href="https://www.instagram.com/jov.andrs/" target="_blank" rel="noreferer">
                     <i style={{ color: '#f16d' }} class="fab fa-instagram"></i>
                 </IconButton>
-                <IconButton href="https://www.linkedin.com/in/jova-andres-riski-b0b232200/" target="_blank" rel="noreferer">
+                <IconButton className="social" href="https://www.linkedin.com/in/jova-andres-riski-b0b232200/" target="_blank" rel="noreferer">
                     <i style={{ color: '#1a9ddb' }} class="fab fa-linkedin"></i>
                 </IconButton>
-                <IconButton href="https://open.spotify.com/user/389di68eunjgi5z3q680y7y87" target="_blank" rel="noreferer">
+                <IconButton className="social" href="https://open.spotify.com/user/389di68eunjgi5z3q680y7y87" target="_blank" rel="noreferer">
                     <i style={{ color: '#17e660' }} class="fab fa-spotify"></i>
                 </IconButton>
             </div>
             <div className="credit">
-                <p>&copy; 2021 by jova andreas</p>
+                <span></span>
             </div>
         </footer>
     )
